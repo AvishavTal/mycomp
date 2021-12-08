82f6f1d3ff07eca89d8827f1343a5debff036702
Avishav Tal <avishavtal@gmail.com>
2021-12-08 13:42:29 +0200

    implementaions of read_com and print_comp

--- complex.c
+++ complex.c
@@ -1,2 +1,47 @@
 
+#include <stdlib.h>
+#include <stdio.h>
 #include "complex.h"
+struct complex{
+    float real;
+    float img;
+};
+
+/*initial complex number*/
+complex read_com(float real, float img){
+    complex result;
+    result=(complex) malloc(sizeof (struct complex));
+    result->real=real;
+    result->img=img;
+    return result;
+}
+
+/*get a complex number to print and print it*/
+void print_comp(complex to_print){
+    printf("%f",to_print->real);
+    if (to_print->img!=0){
+        if (to_print->img>0){
+            printf("%c",'+');
+        }
+        printf("%fi",to_print->img);
+    }
+    printf("\n");
+}
+
+/*get tow complex numbers and return the result of adding them together*/
+complex add_comp(complex complex1, complex complex2);
+
+/*get tow complex numbers and return the result of subtraction from each other*/
+complex sub_comp(complex complex1,complex complex2);
+
+/*get complex number and real number and return their multiplication result*/
+complex mult_comp_real(complex complex1, float real);
+
+/*get complex number and imaginary number and return their multiplication result*/
+complex mult_comp_img(complex complex1, float img);
+
+/*get tow complex numbers and return their multiplication result*/
+complex mult_comp_comp(complex complex1, complex complex2);
+
+/*get a complex number and return its absolute value*/
+float abs_comp(complex complex1);
--- complex.h
+++ complex.h
@@ -1,10 +1,10 @@
-typedef struct complex_struct{
-    int real;
-    int img;
-}complex;
-
+//typedef struct complex_struct{
+//    int real;
+//    int img;
+//}complex;
+typedef struct complex *complex;
 /*initial complex number*/
-complex *read_com(int real,int img);
+complex read_com(float real, float img);
 
 /*get a complex number to print and print it*/
 void print_comp(complex to_print);
@@ -16,12 +16,13 @@ complex add_comp(complex complex1, complex complex2);
 complex sub_comp(complex complex1,complex complex2);
 
 /*get complex number and real number and return their multiplication result*/
-complex mult_comp_real(complex complex1,int real);
+complex mult_comp_real(complex complex1, float real);
 
 /*get complex number and imaginary number and return their multiplication result*/
-complex mult_comp_img(complex complex1,int img);
+complex mult_comp_img(complex complex1, float img);
 
 /*get tow complex numbers and return their multiplication result*/
 complex mult_comp_comp(complex complex1, complex complex2);
 
 /*get a complex number and return its absolute value*/
+float abs_comp(complex complex1);
